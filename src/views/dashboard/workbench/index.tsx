2df42834cbbfd63a85b0a1502d8450ac80ee53b3
liujie_278 <liujie_278@163.com>
2023-12-29 11:54:45 +0800

    feat: 不知道新增了什么

--- src/views/dashboard/workbench/index.tsx
+++ src/views/dashboard/workbench/index.tsx
@@ -1,32 +1,12 @@
 import { Button } from 'antd'
-import userService, { SignInReq } from '@/api/services/userService'
-import { useMutation } from '@tanstack/react-query'
-import { useState } from 'react'
 
 const Workbench = () => {
-  const [flag, setFlag] = useState(false)
-  const data: SignInReq = {
-    username: 'admin',
-    password: '123456',
-  }
-  const handleClick = () => {
-    setFlag(!flag)
-    // console.log('click', userService.signin(data))
-    //
-    // const signInMutation = useMutation(userService.signin)
-    // console.log(signInMutation.mutateAsync(data))
-  }
-
-  const handleClickMutation = () => {
-    const signInMutation = useMutation({
-      mutationFn: userService.signin,
-    })
-    console.log(signInMutation.mutateAsync(data))
-  }
+  const handleClick = () => {}
+  const handleClickMutation = () => {}
 
   return (
     <>
-      <div>Workbench {flag ? 'true' : 'false'} </div>
+      <div>Workbench </div>
       <Button onClick={handleClick}>Click</Button>
       <Button onClick={handleClickMutation}>Click Mutation</Button>
     </>
